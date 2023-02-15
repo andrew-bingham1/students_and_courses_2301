3b60541cab03797a0d333b3c598b2e891ccaa3d3
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-15 09:18:45 -0700

    test: create scores test

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -21,4 +21,11 @@ RSpec.describe do
       expect(student.age).to eq(21)
     end
   end
+
+  describe '#scores' do
+    it 'can check scores'
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student.scores).to eq([])
+  end
 end
