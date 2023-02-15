1a6fc9d369cf7aadf24b25b877266ec9782e7e96
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-15 09:23:25 -0700

    feat: can log scores

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,14 @@
 class Student
 attr_reader :details, :name, :age, :scores
-# attr_accessor :scores
   def initialize(details)
    @details = details
    @name = details.values.first
    @age = details.values.last
    @scores = []
   end
+
+  def log_score(score)
+    @scores << score
+  end
+
 end
