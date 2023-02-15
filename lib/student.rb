628458be703d1f30047a0d13d8e110a5012a77a5
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-15 09:15:47 -0700

    feat: added to initialize method

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,8 @@
 class Student
+attr_reader :details, :name, :age
   def initialize(details)
-  
+   @details = details
+   @name = details.values.first
+   @age = details.values.last
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe do
 
       expect(student).to be_an_instance_of Student
     end
+
+    it 'has a name' do 
+      student = Student.new({name: "Morgan", age: 21}) 
+      
+      expect(student.name).to eq("Morgan")
+
+    end
   end
 end
