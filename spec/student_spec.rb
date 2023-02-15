455fc6578a056a65c0e98b4404241beb8f34ed25
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-15 09:17:07 -0700

    test: add student age test

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -11,9 +11,14 @@ RSpec.describe do
 
     it 'has a name' do 
       student = Student.new({name: "Morgan", age: 21}) 
-      
-      expect(student.name).to eq("Morgan")
 
+      expect(student.name).to eq("Morgan")
+    end
+
+    it 'has an age' do
+      student = Student.new({name: "Morgan", age: 21})
+      
+      expect(student.age).to eq(21)
     end
   end
 end
