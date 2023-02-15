a1975ef6c90b0495b36cf5556f05b89fdcda0fd4
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-15 09:20:45 -0700

    feat: can check scores

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,10 @@
 class Student
-attr_reader :details, :name, :age
+attr_reader :details, :name, :age, :scores
+# attr_accessor :scores
   def initialize(details)
    @details = details
    @name = details.values.first
    @age = details.values.last
+   @scores = []
   end
 end
